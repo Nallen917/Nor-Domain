4036df69cf4c03b68b800834ab8fe5db3e1cd71f
Nor <Norward.Allen@tlgcohort.com>
2023-07-19 21:54:36 +0000

    mini project

--- iftest2/ipask2.py
+++ iftest2/ipask2.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-ipchk = input("Apply an IP address: ") # this line now prompts the user for input
+ipchk = input("What house are you in GOT:") # this line now prompts the user for input
 
-# if user set IP of gateway
-if ipchk == "192.168.70.1":
-   print("Looks like the IP address of the Gateway was set: " + ipchk + " This is not recommended.")
-elif ipchk: # if any data is provided, this will test true
-   print("Looks like the IP address was set: " + ipchk) # indented under if
-else: # if data is NOT provided
-   print("You did not provide input.") # indented under else
+# check if ipchk does NOT have a value
+while ipchk == "":
+    ipchk = input("What house are you in GOT:") # this line now prompts the user for input
 
+# if user sets Preffered House
+if ipchk == "house lanister":
+   print("Looks like House Lanister of Casterly Rock was set: " + ipchk + " This is not recommended!!")
+elif ipchk == "house stark":
+    print("house stark lives on🐺")
+elif ipchk == "house targaryen":
+    print("Dracarys 🔥🔥🔥🐉")
+else: # if any data is provided, this will test true
+    print("Looks like The Mother of Dragons, Queen of the unsully, breaker of chains will accept your response") 
+#e: # if data is NOT provided
+ #  print("You did not provide input.n\ Dracarys") # indented under else
